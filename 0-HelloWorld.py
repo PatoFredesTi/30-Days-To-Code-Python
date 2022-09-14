8439a53979deb19a7a6c3e04d6ef6cfdfea6d62c
Patricio Fredes <59983502+PatoFredesTi@users.noreply.github.com>
2022-09-14 15:22:40 -0300

    Create 0-HelloWorld.py

---
+++ 0-HelloWorld.py
@@ -0,0 +1,8 @@
+
+input_string = input()
+
+# imprime una cadena string que diga "Hello, World.".
+print('Hello, World.')
+
+# Escribe una linea de codigo para imprimir el contenido de input_string.
+print(input_string)
