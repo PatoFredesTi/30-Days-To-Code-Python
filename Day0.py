d80b14797da90cc84ac67b25ebe48eed092d0fcf
Patricio Fredes <59983502+PatoFredesTi@users.noreply.github.com>
2022-09-14 15:15:32 -0300

    Create Day0.py

---
+++ Day0.py
@@ -0,0 +1,6 @@
+#Dia 0, Inicio del Desafio 30 dias de Codigo Python.
+#Nota: En la primera linea ya estaba el "Hello, World"
+#El objetivo es imprimir por pantalla: "Welcome to 30 Days of Code!"
+
+print("Hello, World.")
+print("Welcome to 30 Days of Code!")
